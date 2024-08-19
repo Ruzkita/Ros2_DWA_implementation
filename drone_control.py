c3da0d78f98da9da2e514af650fe1db3f162d4f1
Lucas Leonardo <lucasle58022@gmail.com>
2024-08-19 19:13:01 -0300

    Update drone_control.py

--- drone_control.py
+++ drone_control.py
@@ -41,39 +41,74 @@ class Drone(Node):
         
 
         #PUBLISHERS_START
+
         self.vehicle_command_publisher = self.create_publisher(VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
+        self.trajectory_setpoint_publisher = self.create_publisher(TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
+        self.offboard_control_mode_publisher = self.create_publisher(OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
 
         #PUBLISHERS_END
 
-        self.timer = self.create_timer(0.1, self.vehicle_command)
+        #SUBSCRIBERS_START
 
-        self.arm = self.vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1= 1.0)
+        self.vehicle_status_subscriber = self.create_subscription(VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile)
+        self.vehicle_local_position_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
 
+        #SUBSCRIBERS_END
+
+        self.vehicle_status = VehicleStatus()
+        self.vehicle_local_position = VehicleLocalPosition()
+        self.timer = self.create_timer(0.1, self.timer_callback)
     
 
-    def vehicle_command(self, command, **params):
+    def vehicle_command(self, command, **params) -> None:
         msg = VehicleCommand()
         msg.command = command
         msg.param1 = params.get("param1", 0.0)
+        msg.param2 = params.get("param2", 0.0)
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.vehicle_command_publisher.publish(msg)
-
-
-
-
-
-    """def user_interface(self):
-        input("X: ", self.x)
-        input("Y: ", self.y)
-        input("Z: ", self.z)
-        position_setpoint(self.x, self.y, self.z)
-
-    def position_setpoint(self, x:float, y:float, z:float):
-        msg = TrajectorySetpoint
-        msg.position = [x,y,z]
-        msg.yaw = 1.57079  
-        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)"""
     
+    def arm_vehicle(self):
+        self.vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1= 1.0)
+    
+    def engange_offboard(self):
+        self.vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1 = 1.0, param2 = 6.0)
+        #self.get_logger().info("Entrando em modo offboar")
+    
+    def trajectory_setpoint(self, x:float, y:float, z:float):
+        msg = TrajectorySetpoint()
+        msg.position = [x, y, z]
+        msg.yaw = 1.57079
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.trajectory_setpoint_publisher.publish(msg)
+        self.get_logger().info(f"Indo para {[x, y, z]}")
+
+    def vehicle_status_callback(self, vehicle_status):
+        self.vehicle_status = vehicle_status
+    
+    def vehicle_local_position_callback(self, vehicle_local_position):
+        self.vehicle_local_position = vehicle_local_position
+    
+    def offboard_control_mode(self):
+        msg = OffboardControlMode()
+        msg.position = True
+        msg.velocity = False
+        msg.acceleration = False
+        msg.attitude = False
+        msg.body_rate = False
+        msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
+        self.offboard_control_mode_publisher.publish(msg)
+
+
+
+    def user_interface(self):
+        self.engange_offboard()
+        self.arm_vehicle()
+        self.trajectory_setpoint(0.0, 0.0, -5.0)
+    
+    def timer_callback(self) -> None:
+        self.offboard_control_mode()
+        self.user_interface()
 
 
 def main (args = None) -> None:
